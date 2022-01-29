7a40cc2a0c716554b0709a647447d277c8cb9562
NoshinTahsin <35559870+NoshinTahsin@users.noreply.github.com>
2022-01-29 22:14:54 +0600

    component for inventory management

---
+++ ClassLibraryInventory/ClassLibraryInventory/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassLibraryInventory
+{
+    interface IInventory
+    {
+        public void check_inventory();
+     
+    }
+
+    public class Inventory : IInventory
+    {
+        public void check_inventory()
+        {
+            Console.WriteLine("Checking inventory for the target book...");
+            Console.WriteLine("The target book is available...");
+        }
+
+    }
+}
