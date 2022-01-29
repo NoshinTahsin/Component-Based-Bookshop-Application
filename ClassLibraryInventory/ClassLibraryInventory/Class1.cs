83b103f38873e956b8e1e47b3be888271f290b03
NoshinTahsin <35559870+NoshinTahsin@users.noreply.github.com>
2022-01-29 23:12:06 +0600

    inventory component

--- ClassLibraryInventory/ClassLibraryInventory/Class1.cs
+++ ClassLibraryInventory/ClassLibraryInventory/Class1.cs
@@ -12,8 +12,10 @@ namespace ClassLibraryInventory
     {
         public void check_inventory()
         {
-            Console.WriteLine("Checking inventory for the target book...");
-            Console.WriteLine("The target book is available...");
+            Console.WriteLine("Checking inventory for the target product...");
+            Console.WriteLine();
+            Console.WriteLine("The target product is available...");
+            Console.WriteLine();
         }
 
     }
